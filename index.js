5c1dee56006edb166deab14bc8acdf0f084b9902
mysterlune <mysterlune@gmail.com>
2013-04-04 10:15:52 -0700

    Adding accessors get/set with path compatibility

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+
+Hoser stands in as an middleman between client applications of a Node.js / Express
+application, brokering client requests for remote resources.
+
+Remote resources must be described using the mappable mixin.
+
+*/
+module.exports = require('./lib/index');
